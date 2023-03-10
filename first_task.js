c059ac859eef29789fb5d9b8ccf220884ca1aaee
Travkina_Olga <otravushka70@mail>
2023-03-10 16:51:02 +0300

    Выполнила 1 задачу из 1 задания

--- first_task.js
+++ first_task.js
@@ -1,2 +1,7 @@
 'use strict';
 
+let km_per_hour = 36;
+let m_per_sec = 20;
+
+console.log(`${km_per_hour} км/ч соответствует ${km_per_hour / 3.6} м/с`);
+console.log(`${m_per_sec} м/с соответствует ${m_per_sec * 3.6} км/ч`);
