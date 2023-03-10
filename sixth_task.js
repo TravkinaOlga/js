6dc9f96e0ebc858266a0b39fb42f829318b35a24
Travkina_Olga <otravushka70@mail>
2023-03-10 23:08:14 +0300

    Выполнила и протестировала на странице в браузере 6 задачу из 1 задания

---
+++ sixth_task.js
@@ -0,0 +1,11 @@
+'use strict'
+
+let n = prompt('Введите делимое', 0);
+let x = prompt('Введите первый делитель', 0);
+let y = prompt('Введите второй делитель', 0);
+
+let flag = true;
+if (n % x != 0|| n % y != 0) {
+    flag = false;
+}
+alert(`n = ${n}, x = ${x}, y = ${y} => ${flag}`);
