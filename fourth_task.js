acaf8e194633aafdf4d1bc4220aa871e3b793bd7
Travkina_Olga <otravushka70@mail>
2023-03-10 19:55:06 +0300

    Сделала 4 задачу в 1 задании, ещё не разобралась как выводить не только на консолль, но и в браузере, но не через alert

--- fourth_task.js
+++ fourth_task.js
@@ -1,2 +1,20 @@
 'use strict'
 
+//let n = prompt('Сколько слоёв в Вашей ёлке?', 0);
+let n = 13;
+
+let z = '*';
+let r = '#';
+let index;
+for (let i = 0; n != i - 1; i++) {
+    if (i === 0) {
+        index = '>';
+    } else if (i === n) {
+        index = '||';
+    } else if (i % 2 != 0) {
+        index = z.repeat(i);
+    } else {
+        index = r.repeat(i);
+    }
+    process.stdout.write(`${index}\n`);
+}
