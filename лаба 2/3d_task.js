fa1e23ce80f04e6468810feb68397279348a53ee
Travkina_Olga <otravushka70@mail>
2023-03-24 21:25:56 +0300

    забыла подключить к html доку

--- лаба 2/3d_task.js
+++ лаба 2/3d_task.js
@@ -1,12 +1,15 @@
+'use strict'
+
 let student = {
     group: "211-325",
     last_name: "Травкина",
     first_name: "Ольга",
 };
 
-console.log('Список свойств:');
+let array = [];
 for (let key in student) {
-    console.log(key);
+    array.push(key);
 }
 
+console.log(`Список свойств: ${array.join(', ')}`);
 console.log(`Студент ${student.last_name} ${student.first_name} учится в ${student.group} группе`);
